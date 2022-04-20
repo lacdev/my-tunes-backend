bfdbce2774f3ab8f44bbf8dbe5ee5654fcff6897
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-19 22:23:23 -0500

    add try catch handlers for artists controllers

--- src/controllers/artist.ts
+++ src/controllers/artist.ts
@@ -7,32 +7,51 @@ import {
 } from '../usecases/artist'
 
 export const handleGetArtists = async (req, res) => {
-  const artists = await getAllArtists()
-
-  res.send({ response: artists })
+  try {
+    const artists = await getAllArtists()
+    res.send({ response: artists })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGetArtist = async (req, res) => {
-  const { artistId } = req.params
-  const artist = await getArtistById(artistId)
-  res.send({ response: artist })
+  try {
+    const { artistId } = req.params
+    const artist = await getArtistById(artistId)
+    res.send({ response: artist })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleArtistCreate = async (req, res) => {
-  const body = req.body
-  const artist = await createArtist(body)
-  res.send({ status: 200, response: artist })
+  try {
+    const body = req.body
+    const artist = await createArtist(body)
+    res.send({ status: 200, response: artist })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleArtistUpdate = async (req, res) => {
-  const { artistId } = req.params
-  const body = req.body
-  const artist = await updateArtist(artistId, body)
-  res.send({ status: 200, response: artist })
+  try {
+    const { artistId } = req.params
+    const body = req.body
+    const artist = await updateArtist(artistId, body)
+    res.send({ status: 200, response: artist })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleArtistDelete = async (req, res) => {
-  const { artistId } = req.params
-  const artist = await deleteArtist(artistId)
-  res.send({ status: 204, response: artist })
+  try {
+    const { artistId } = req.params
+    const artist = await deleteArtist(artistId)
+    res.send({ status: 204, response: artist })
+  } catch (e) {
+    console.error(e)
+  }
 }
