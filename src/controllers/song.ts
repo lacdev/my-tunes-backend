8248c332d4d0d3d7f6d4f76d7484dc57bdf119e9
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-19 22:26:21 -0500

    add try catch handlers for songs controllers

--- src/controllers/song.ts
+++ src/controllers/song.ts
@@ -7,31 +7,51 @@ import {
 } from '../usecases/song'
 
 export const handleGetSongs = async (req, res) => {
-  const songs = await getAllSongs()
-  res.send({ response: songs })
+  try {
+    const songs = await getAllSongs()
+    res.send({ response: songs })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGetSong = async (req, res) => {
-  const { songId } = req.params
-  const song = await getSongById(songId)
-  res.send({ response: song })
+  try {
+    const { songId } = req.params
+    const song = await getSongById(songId)
+    res.send({ response: song })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleSongCreate = async (req, res) => {
-  const body = req.body
-  const song = await createSong(body)
-  res.send({ status: 200, response: song })
+  try {
+    const body = req.body
+    const song = await createSong(body)
+    res.send({ status: 200, response: song })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleSongUpdate = async (req, res) => {
-  const { songId } = req.params
-  const body = req.body
-  const song = await updateSong(songId, body)
-  res.send({ status: 200, response: song })
+  try {
+    const { songId } = req.params
+    const body = req.body
+    const song = await updateSong(songId, body)
+    res.send({ status: 200, response: song })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleSongDelete = async (req, res) => {
-  const { songId } = req.params
-  const song = await deleteSong(songId)
-  res.send({ status: 204, response: song })
+  try {
+    const { songId } = req.params
+    const song = await deleteSong(songId)
+    res.send({ status: 204, response: song })
+  } catch (e) {
+    console.error(e)
+  }
 }
