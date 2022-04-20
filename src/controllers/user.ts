e67bc91c716beb85851033ad34069a5f7fb52e7a
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-19 22:27:37 -0500

    add try catch handlers for user controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -7,31 +7,51 @@ import {
 } from '../usecases/user'
 
 export const handleGetUsers = async (req, res) => {
-  const users = await getAllUsers()
-  res.send({ response: users })
+  try {
+    const users = await getAllUsers()
+    res.send({ response: users })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGetUser = async (req, res) => {
-  const { userId } = req.params
-  const user = await getUserById(userId)
-  res.send({ response: user })
+  try {
+    const { userId } = req.params
+    const user = await getUserById(userId)
+    res.send({ response: user })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleUserCreate = async (req, res) => {
-  const body = req.body
-  const user = await createUser(body)
-  res.send({ status: 200, response: user })
+  try {
+    const body = req.body
+    const user = await createUser(body)
+    res.send({ status: 200, response: user })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleUserUpdate = async (req, res) => {
-  const { userId } = req.params
-  const body = req.body
-  const user = await updateUser(userId, body)
-  res.send({ status: 200, response: user })
+  try {
+    const { userId } = req.params
+    const body = req.body
+    const user = await updateUser(userId, body)
+    res.send({ status: 200, response: user })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleUserDelete = async (req, res) => {
-  const { userId } = req.params
-  const user = await deleteUser(userId)
-  res.send({ status: 204, response: user })
+  try {
+    const { userId } = req.params
+    const user = await deleteUser(userId)
+    res.send({ status: 204, response: user })
+  } catch (e) {
+    console.error(e)
+  }
 }
