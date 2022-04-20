a52756505f5200f2c490f547219dd03c5778d2a3
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-19 22:24:47 -0500

    add try catch handlers for genre controllers

--- src/controllers/genre.ts
+++ src/controllers/genre.ts
@@ -7,33 +7,51 @@ import {
 } from '../usecases/genre'
 
 export const handleGetGenres = async (req, res) => {
-  const genres = await getAllGenres()
-  res.send({ response: genres })
+  try {
+    const genres = await getAllGenres()
+    res.send({ response: genres })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGetGenre = async (req, res) => {
-  const { genreId } = req.params
-  const genre = await getGenreById(genreId)
-
-  console.log(genre)
-  res.send({ response: genre })
+  try {
+    const { genreId } = req.params
+    const genre = await getGenreById(genreId)
+    res.send({ response: genre })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGenreCreate = async (req, res) => {
-  const body = req.body
-  const genre = await createGenre(body)
-  res.send({ status: 200, response: genre })
+  try {
+    const body = req.body
+    const genre = await createGenre(body)
+    res.send({ status: 200, response: genre })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGenreUpdate = async (req, res) => {
-  const { genreId } = req.params
-  const body = req.body
-  const genre = await updateGenre(genreId, body)
-  res.send({ status: 200, response: genre })
+  try {
+    const { genreId } = req.params
+    const body = req.body
+    const genre = await updateGenre(genreId, body)
+    res.send({ status: 200, response: genre })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export const handleGenreDelete = async (req, res) => {
-  const { genreId } = req.params
-  const genre = await deleteGenre(genreId)
-  res.send({ status: 204, response: genre })
+  try {
+    const { genreId } = req.params
+    const genre = await deleteGenre(genreId)
+    res.send({ status: 204, response: genre })
+  } catch (e) {
+    console.error(e)
+  }
 }
