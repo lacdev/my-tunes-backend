c601fa503d8c80b3aa5c3155dda2aac33527bfc6
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-19 22:13:33 -0500

    add song handlers

--- src/controllers/song.ts
+++ src/controllers/song.ts
@@ -1,30 +1,37 @@
-export const handleGetGenres = async (req, res) => {
-  const genres = await getAllGenres()
+import {
+  createSong,
+  deleteSong,
+  getAllSongs,
+  getSongById,
+  updateSong,
+} from '../usecases/song'
 
-  res.send({ response: genres })
+export const handleGetSongs = async (req, res) => {
+  const songs = await getAllSongs()
+  res.send({ response: songs })
 }
 
-export const handleGetGenre = async (req, res) => {
-  const { genreId } = req.params
-  const genre = await getGenreById(genreId)
-  res.send({ response: genre })
+export const handleGetSong = async (req, res) => {
+  const { songId } = req.params
+  const song = await getSongById(songId)
+  res.send({ response: song })
 }
 
-export const handleGenreCreate = async (req, res) => {
+export const handleSongCreate = async (req, res) => {
   const body = req.body
-  const genre = await createGenre(body)
-  res.send({ status: 200, response: genre })
+  const song = await createSong(body)
+  res.send({ status: 200, response: song })
 }
 
-export const handleGenreUpdate = async (req, res) => {
-  const { genreId } = req.params
+export const handleSongUpdate = async (req, res) => {
+  const { songId } = req.params
   const body = req.body
-  const genre = await updateGenre(genreId, body)
-  res.send({ status: 200, response: genre })
+  const song = await updateSong(songId, body)
+  res.send({ status: 200, response: song })
 }
 
-export const handleGenreDelete = async (req, res) => {
-  const { genreId } = req.params
-  const genre = await deleteGenre(genreId)
-  res.send({ status: 204, response: genre })
+export const handleSongDelete = async (req, res) => {
+  const { songId } = req.params
+  const song = await deleteSong(songId)
+  res.send({ status: 204, response: song })
 }
--- src/routes/song.ts
+++ src/routes/song.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
+import {
+  handleGetSong,
+  handleGetSongs,
+  handleSongCreate,
+  handleSongDelete,
+  handleSongUpdate,
+} from '../controllers/song'
 
 const router = Router()
 
-router.get('/')
-router.get('/:songId')
-router.post('/')
-router.patch('/:songId')
-router.delete('/:songId')
+router.get('/', handleGetSongs)
+router.get('/:songId', handleGetSong)
+router.post('/', handleSongCreate)
+router.patch('/:songId', handleSongUpdate)
+router.delete('/:songId', handleSongDelete)
 
 export { router as songRouter }
